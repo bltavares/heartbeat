0c8e4657d5a7259ac00a6634461e3ae65f22a4d8
Bruno Tavares <connect+github@bltavares.com>
2016-03-28 08:48:14 -0300

    Retrieves the information from the URL

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+extern crate hyper;
 extern crate clap;
+
+use std::io::Read;
+
 use clap::{App, Arg};
 
+use hyper::Client;
+use hyper::header::Connection;
+
 fn main() {
-  App::new("heartbeat")
+  let matches = App::new("heartbeat")
       .version("v0.1.0-beta")
       .arg(Arg::with_name("url")
            .long("url")
@@ -11,4 +18,19 @@ fn main() {
            .value_name("URL")
            .required(true))
       .get_matches();
+
+  request(matches.value_of("url").expect("URL not present"));
+}
+
+fn request(url: &str) {
+    let client = Client::new();
+
+    let mut response = client.get(url)
+        .header(Connection::close())
+        .send().expect("Could not make request");
+
+    let mut body = String::new();
+    response.read_to_string(&mut body).expect("Could not read the response into a buffer");
+
+    println!("Response: {}", body);
 }
