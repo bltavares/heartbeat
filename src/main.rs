26328f52936acfd6bd299724ebfb43a446a53f97
Bruno Tavares <connect+github@bltavares.com>
2016-03-28 08:37:38 -0300

    Requires an URL as argument of the application

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 extern crate clap;
-use clap::App;
+use clap::{App, Arg};
 
 fn main() {
-  App::new("heartbeat").version("v0.1.0-beta").get_matches();
+  App::new("heartbeat")
+      .version("v0.1.0-beta")
+      .arg(Arg::with_name("url")
+           .long("url")
+           .index(1)
+           .takes_value(true)
+           .value_name("URL")
+           .required(true))
+      .get_matches();
 }
