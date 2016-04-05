410ffb3baee2f14c5c26326b6853adba9c25d08a
Bruno Tavares <connect+github@bltavares.com>
2016-04-05 17:08:32 -0300

    Formats the code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ extern crate time;
 
 use clap::{App, Arg};
 
-use stopwatch::{Stopwatch};
+use stopwatch::Stopwatch;
 use time::Duration;
 
 use hyper::Client;
@@ -20,14 +20,14 @@ struct MeasuredResponse {
 
 fn main() {
     let matches = App::new("heartbeat")
-        .version("v0.1.0-beta")
-        .arg(Arg::with_name("url")
-             .long("url")
-             .index(1)
-             .takes_value(true)
-             .value_name("URL")
-             .required(true))
-        .get_matches();
+                      .version("v0.1.0-beta")
+                      .arg(Arg::with_name("url")
+                               .long("url")
+                               .index(1)
+                               .takes_value(true)
+                               .value_name("URL")
+                               .required(true))
+                      .get_matches();
 
     let measured_response = request(matches.value_of("url").expect("URL not present"));
     println!("{:?}", measured_response);
@@ -38,7 +38,7 @@ fn request(url: &str) -> MeasuredResponse {
     client.set_read_timeout(Some(std::time::Duration::from_secs(10)));
 
     let request = client.get(url)
-        .header(Connection::close());
+                        .header(Connection::close());
 
     let stop_watch = Stopwatch::start_new();
     let response = request.send().expect("Could not make request");
