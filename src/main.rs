be8aac9a454baad69a6d0465c61aac9e69ed8135
Bruno Tavares <connect+github@bltavares.com>
2016-04-06 16:16:42 -0300

    Extracts the logic to parse the arguments into an ApplicationConfiguration into a function

--- src/main.rs
+++ src/main.rs
@@ -20,37 +20,7 @@ struct ApplicationConfiguration {
 }
 
 fn main() {
-    let interval_help_message = format!("The interval in seconds between requests, default to {} \
-                                         seconds",
-                                        DEFAULT_INTERVAL_IN_SECONDS);
-    let matches = App::new("heartbeat")
-                      .version("v0.1.0-beta")
-                      .arg(Arg::with_name("url")
-                               .long("url")
-                               .index(1)
-                               .takes_value(true)
-                               .value_name("URL")
-                               .help("The URL to monitor")
-                               .required(true))
-                      .arg(Arg::with_name("interval")
-                               .long("interval")
-                               .short("i")
-                               .takes_value(true)
-                               .value_name("INTERVAL")
-                               .help(&interval_help_message))
-                      .get_matches();
-
-
-    let interval_argument = matches.value_of("interval")
-                                   .map(|arg| {
-                                       u64::from_str(arg)
-                                           .expect("The interval argument requires a number")
-                                   });
-
-    let application_configuration = ApplicationConfiguration {
-        url: matches.value_of("url").expect("URL not present").to_string(),
-        interval: Duration::from_secs(interval_argument.unwrap_or(DEFAULT_INTERVAL_IN_SECONDS)),
-    };
+    let application_configuration = parse_arguments();
 
     loop {
         let measured_response = MeasuredResponse::request(&application_configuration.url);
@@ -64,4 +34,37 @@ fn display(response: &MeasuredResponse) {
     let duration = response.time;
     let url = response.url();
     println!("{} -> Status: {}, time: {}s", url, status, duration);
+}
+
+fn parse_arguments() -> ApplicationConfiguration {
+    let interval_help_message = format!("The interval in seconds between requests, default to {} \
+                                         seconds",
+                                        DEFAULT_INTERVAL_IN_SECONDS);
+
+    let cli_arguments = App::new("heartbeat")
+                            .version("v0.1.0-beta")
+                            .arg(Arg::with_name("interval")
+                                     .long("interval")
+                                     .short("i")
+                                     .takes_value(true)
+                                     .value_name("INTERVAL")
+                                     .help(&interval_help_message))
+                            .arg(Arg::with_name("url")
+                                     .long("url")
+                                     .index(1)
+                                     .takes_value(true)
+                                     .value_name("URL")
+                                     .help("The URL to monitor")
+                                     .required(true))
+                            .get_matches();
+
+
+    let interval_argument = cli_arguments.value_of("interval").map(|arg| {
+        u64::from_str(arg).expect("The interval argument requires a number")
+    });
+
+    ApplicationConfiguration {
+        url: cli_arguments.value_of("url").expect("URL not present").to_string(),
+        interval: Duration::from_secs(interval_argument.unwrap_or(DEFAULT_INTERVAL_IN_SECONDS)),
+    }
 }
