e86e813434dd4522129c689cf29eacf3f2dd200c
Bruno Tavares <connect+github@bltavares.com>
2016-05-23 23:52:40 -0300

    Fixes interface changes on dependen libraries

--- src/measured_response.rs
+++ src/measured_response.rs
@@ -73,7 +73,7 @@ impl MeasuredResponse {
         match request.send() {
             Err(_) => MeasuredResponse::empty_failure(),
             Ok(response) => {
-                let duration = stop_watch.elapsed();
+                let duration = TimeDuration::from_std(stop_watch.elapsed()).expect("Could not measure elapsed response time");
 
                 MeasuredResponse {
                     status: StatusOrError::Status(response.status),
